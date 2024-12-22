932516427364cf4d555f06c3ce53be9c1c4023ff
Vaibhav Soni <sonivaibhav0470@gmail.com>
2024-12-23 01:21:45 +0530

    Completed Variables and Datatypes in Js

---
+++ Variables_Datatypes/variable.js
@@ -0,0 +1,28 @@
+// Create a const object called "product" to store information
+
+const Product = {
+    title: "Ball Pen",
+    rating: "4",
+    deal: true,
+    price: 270,
+    offer:"5"
+}
+
+// Create a const object called "product" to store information
+console.log("________________________________________________________")
+console.log("")
+
+const profile = {
+    userName : "vaibhav-soni74",
+    posts: "4",
+    isFollow: false,
+        // followers: "null",
+    following:"3",
+    name: "Vaibhav Soni",
+    description:"Full Stack Developer "
+}
+console.log(Product)
+console.log(Product["price"])
+
+console.log(profile)
+console.log(typeof profile["name"])
