87f1e75f28e8275229a0f9eccfc24ff82c54ceda
Vaibhav Soni <sonivaibhav0470@gmail.com>
2024-12-23 01:26:04 +0530

    Renamed Js File

---
+++ ConditionalStatements/divisiblity.js
@@ -0,0 +1,14 @@
+// Get User to input a number . Check if the number is divisible by 5 or not
+
+// let num = prompt("Enter a Number: ");
+
+let num = 352;
+if(num % 5 === 0 ){
+    console.log("Number is divisible by 5")
+} else {
+   console.log("Number is not divisible by 5")
+}
+
+//or 
+
+// (num % 5 === 0) ? console.log(`${num} is divisible by 5`):console.log(`${num} is not divisible by 5`);
