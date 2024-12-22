2d18f826bfd61724d69566ddf58e1c954fe2e713
Vaibhav Soni <sonivaibhav0470@gmail.com>
2024-12-23 01:23:57 +0530

    Completed Conditional Statements in Js

---
+++ ConditionalStatements/score.js
@@ -0,0 +1,15 @@
+// tkae the score from user and print the grade
+
+// let score = prompt ("Enter your Score to Check Grades : ")
+let score = 45;
+if (score > 90 && score <= 100){
+    console.log("Grade A")
+}else if(score < 89 && score >=70){
+    console.log("Grade B")
+}else if(score < 69  && score >=60){
+    console.log("Grade c")
+}else if(score <59 && score >=50){
+    console.log("Grade D")
+}else{
+    console.log("Grade F")
+}
