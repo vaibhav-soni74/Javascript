c36c1f8a1d68856ee0b0f70eff3c71d2587ca1b4
Vaibhav Soni <sonivaibhav0470@gmail.com>
2024-12-29 20:50:48 +0530

    completed Higher Order Functions  in Js

--- HighOrder_Functions/pQ4.js
+++ HighOrder_Functions/pQ4.js
@@ -1,10 +1,4 @@
-// // We are given an array  of marks of students .Filter out the marks of the students that scored 90+
-
-// let marks = [45,59,85,74,90,99,100,65,78,88,92,95,98,89,79,69,55,49,39,29,19,9,99];
-// let highMarks = marks.filter((val) =>{
-//     return val > 90;
-// })
-// console.log(highMarks);
+//  
 
 // take a number as input frim user  create an array of number 1 to n .
 // use reduce method to calculate the sum of all numbers in array
